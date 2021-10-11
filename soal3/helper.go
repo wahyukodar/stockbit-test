2ba3c027abfa02decdab8a9043dc766f97c22348
Wahyu Kodar <wahyukodar@192.168.1.3>
2021-10-11 10:33:38 +0700

    Refactor lagi

--- soal3/helper.go
+++ soal3/helper.go
@@ -4,14 +4,10 @@ import "strings"
 
 func findFirstStringInBracket(str string) string {
 	indexFirstBracketFound := strings.Index(str, "(")
-	if indexFirstBracketFound < 0 {
-		return ""
-	}
-
 	runes := []rune(str)
 	wordsAfterFirstBracket := string(runes[indexFirstBracketFound+1 : len(str)])
 	indexClosingBracketFound := strings.Index(wordsAfterFirstBracket, ")")
-	if indexClosingBracketFound < 0 {
+	if indexFirstBracketFound < 0 || indexClosingBracketFound < 0 {
 		return ""
 	}
 	runes = []rune(wordsAfterFirstBracket)
